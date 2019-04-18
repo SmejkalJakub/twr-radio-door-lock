d581cd948238f780017119b424d6c7e1e6352d2e
Jakub Smejkal <smejkal.jakub99@gmail.com>
2019-04-18 21:29:58 +0200

    Update of application.c

--- app/application.c
+++ app/application.c
@@ -3,6 +3,21 @@
 // LED instance
 bc_led_t led;
 
+// Relay Instance
+bc_module_relay_t relay;
+
+void bc_radio_node_on_state_set(uint64_t *id, uint8_t state_id, bool *state)
+{
+    (void) id;
+
+    if (state_id == BC_RADIO_NODE_STATE_RELAY_MODULE_0)
+    {
+        bc_led_blink(&led, 2);
+        bc_module_relay_set_state(&relay, *state);
+
+        bc_radio_pub_state(BC_RADIO_PUB_STATE_RELAY_MODULE_0, state);
+    }
+}
 
 void application_init(void)
 {
@@ -12,6 +27,9 @@ void application_init(void)
     // Initialize LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
     bc_led_set_mode(&led, BC_LED_MODE_ON);
+
+    // Initialize Relay Module
+    bc_module_relay_init(&relay, 0x3B);
 
     bc_radio_init(BC_RADIO_MODE_NODE_LISTENING);
     bc_radio_pairing_request("wireless-lock", VERSION);
